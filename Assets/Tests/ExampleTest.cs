73da90abb30a57fca41a40be6d784cdfb3db65a8
Angelina <07140714no@gmail.com>
2020-02-17 17:11:37 +0300

    test commit

---
+++ Assets/Tests/ExampleTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class ExampleTest
+{
+    [UnityTest]
+    public IEnumerator ExampleEnumirator()
+    {
+        /**
+         * Тест проверяет работу кнопок при взаимодействии с пользователем
+         */
+        GameObject characterGameObject = MonoBehaviour.Instantiate(
+        Resources.Load<GameObject>("Prefabs/Character"));
+        GameObject logicalInputGameObject = MonoBehaviour.Instantiate(
+            Resources.Load<GameObject>("Prefabs/Logical Input"));
+
+        Character character = characterGameObject.GetComponent<Character>();
+        LogicalInput logicalInput = logicalInputGameObject.GetComponent<LogicalInput>();
+        bool expectedState = !logicalInput.GetState();
+
+        logicalInput.transform.position = character.transform.position;
+
+        yield return new WaitForSeconds(1.0f);
+
+        Assert.AreEqual(expectedState, logicalInput.GetState());
+
+        Object.Destroy(character);
+        Object.Destroy(logicalInput);
+    }
+}
+
