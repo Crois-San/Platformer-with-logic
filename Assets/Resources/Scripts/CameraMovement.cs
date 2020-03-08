86229a901077a4c5870c3cec4e7efd2f6f8c7ce8
Crois-San <ilya-smolnicov@mail.ru>
2020-03-08 23:32:55 +0300

    camera moves with player now

---
+++ Assets/Resources/Scripts/CameraMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private GameObject player;
+    private Vector2 playerPosition;
+    [SerializeField]
+    private float speedModifier;
+    void Start()
+    {
+        player = GameObject.Find("Character");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerPosition = player.transform.position;
+        transform.position = new Vector3(Mathf.Lerp(transform.position.x, playerPosition.x, Time.deltaTime * speedModifier) ,Mathf.Lerp(transform.position.y, playerPosition.y, Time.deltaTime * speedModifier),transform.position.z);
+    }
+}
