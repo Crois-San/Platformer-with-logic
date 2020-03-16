9d0ed60948c93f4a061ee18fe42441100a0f9d26
Angelina <07140714no@gmail.com>
2020-03-16 23:11:12 +0300

    test commit

---
+++ Assets/Tests/ItemsTests.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+
+public class ItemsTests
+{
+    [UnityTest]
+    public IEnumerator PistonActionTest()
+    {
+        // Находит ли поршень дверь под собой и началась ли анимация
+        GameObject pistonGameObject = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/Piston"));
+        GameObject tileGameObject = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/Tile"));
+        GameObject inputGameObject = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/Logical Input"));
+        LogicalPiston piston = pistonGameObject.GetComponent<LogicalPiston>();
+        LogicalInput le1 = inputGameObject.GetComponent<LogicalInput>();
+        
+        // Установка позиций
+        tileGameObject.transform.position = new Vector2(piston.transform.position.x, piston.transform.position.y-2.5F);
+        le1.transform.position = new Vector2(piston.transform.position.x-3.0F, piston.transform.position.y);
+        le1.state = true;
+        piston.SetLE1 = le1;
+
+        yield return new WaitForSeconds(1.0f);
+        
+        // Проверка соединения
+        Assert.IsTrue(piston.getConnected);
+        
+        // Проверка анимации
+        Assert.IsFalse(piston.isNotPlaying);
+    }
+    
+    [UnityTest]
+    public IEnumerator WireActionTest()
+    {
+        GameObject wireGameObject = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/Logical Wire"));
+        GameObject logicalInputGameObject = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/Logical Input"));
+        GameObject logicalAndGameObject = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/Logical And"));
+        LogicalWire wire = wireGameObject.GetComponent<LogicalWire>();
+        LogicalInput input = logicalInputGameObject.GetComponent<LogicalInput>();
+        LogicalAnd and = logicalAndGameObject.GetComponent<LogicalAnd>();
+        
+        // Установка позиций
+        and.transform.position = new Vector2(input.transform.position.x+5.0F, input.transform.position.y);
+        // Привязка провода
+        wire.SetLE1 = input;
+        wire.SetLE2 = and;
+        
+        yield return new WaitForSeconds(2.0f);
+        
+        Assert.IsTrue(wire.state);
+
+
+        
+    }
+
+    [UnityTest]
+    public IEnumerator Test()
+    {
+        // Работоспособность логических элементов
+        GameObject logicalInputGameObject = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/Logical Input"));
+        GameObject logicalNotGameObject = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/Logical Not"));
+        LogicalNot not = logicalNotGameObject.GetComponent<LogicalNot>();
+        LogicalInput i = logicalInputGameObject.GetComponent<LogicalInput>();
+        i.transform.position = new Vector2(not.transform.position.x-5.0F, not.transform.position.y);
+        i.state = true;
+
+
+        // ОТРИЦАНИЕ
+        not.SetLE1 = i;
+        yield return new WaitForSeconds(2.0f);
+        Assert.IsFalse(not.state);
+
+        // И
+        GameObject logicalAndGameObject = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/Logical And"));
+        LogicalAnd and = logicalAndGameObject.GetComponent<LogicalAnd>();
+        and.SetLE1 = i;
+        and.SetLE2 = not;
+        yield return new WaitForSeconds(2.0f);
+        Assert.IsFalse(and.state);
+        Object.Destroy(logicalAndGameObject);
+        Object.Destroy(and);
+    }
+}
+
