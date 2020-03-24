210a068035ae0fcbd97e09e932c74425d7ad34a0
Crois-San <ilya-smolnicov@mail.ru>
2020-03-25 00:00:30 +0300

    Level Select menu modifications. Fixed dropdowns

---
+++ Assets/Resources/Scripts/UI Scripts/LevelSelector.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelSelector : MonoBehaviour
+{
+    [field: SerializeField]
+    public GameObject levelHolder { get; set; }
+
+    [field: SerializeField]
+    public GameObject levelIcon { get; set; }
+    [field: SerializeField]
+    public GameObject thisCanvas { get; set; }
+
+    
+    public int numberOfLevels { get; set; } = 50;
+    // Start is called before the first frame update
+    void Start()
+    {
+        var panelDimensions = levelHolder.GetComponent<RectTransform>().rect;
+        var iconDimensions = levelIcon.GetComponent<RectTransform>().rect;
+        var maxInARow = Mathf.FloorToInt(panelDimensions.width / iconDimensions.width);
+        var maxInACol = Mathf.FloorToInt(panelDimensions.height / iconDimensions.height);
+        LoadIcons(numberOfLevels, levelHolder);
+    }
+
+    void LoadIcons(int numberOfIcons, GameObject parentObject)
+    {
+        for (int i = 0; i < numberOfIcons; i++)
+        {
+            var icon = Instantiate(levelIcon, thisCanvas.transform, false);
+            icon.transform.SetParent(parentObject.transform, false);
+            icon.name = "level" + i;
+            icon.GetComponentInChildren<Text>().text= (i+1).ToString();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Resources/Scripts/UI Scripts/LevelUnlockSpriteChange.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelUnlockSpriteChange : MonoBehaviour
+{
+    [field: SerializeField]
+    public bool isUnlocked { get; set; }
+
+    [field: SerializeField]
+    public Sprite unlocked { get; set; }
+    [field: SerializeField]
+    public Sprite locked { get; set; }
+
+    [field: SerializeField]
+    public Image img { get; set; }
+
+    [field: SerializeField]
+    public Text levelNumber { get; set; }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isUnlocked)
+        {
+            img.sprite = unlocked;
+            levelNumber.color = new Color(0.6f,0.0235f,0.0235f); //99 06 06
+        }
+        else
+        {
+            img.sprite = locked;
+            levelNumber.color = new Color(0.9f,0.035f,0.035f); //E6 09 09
+            
+        }
+    }
+}
