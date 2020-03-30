ea1958fc87f39ddbf5648730fdee5ab28be86bbb
Crois-San <ilya-smolnicov@mail.ru>
2020-03-30 18:22:29 +0300

    added working HP bar

---
+++ Assets/Resources/Scripts/UI Scripts/HPBarHPDisplay.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HPBarHPDisplay : MonoBehaviour
+{
+    [field: SerializeField]
+    public Image img { get; set; }
+    [field: SerializeField]
+    public bool hasHP { get; set; }
+    
+    [field: SerializeField]
+    public Sprite HPBarFull { get; set; }
+    [field: SerializeField]
+    public Sprite HPBarEmpty { get; set; }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hasHP)
+        {
+            img.sprite = HPBarFull;
+        }
+        else
+        {
+            img.sprite = HPBarEmpty;
+        }
+    }
+}
---
+++ Assets/Resources/Scripts/UI Scripts/HealthBarInit.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBarInit : MonoBehaviour
+{
+    private int HP;
+    private int currentHP;
+    [field: SerializeField]
+    public GameObject HPBarStart { get; set; }
+    [field: SerializeField]
+    public GameObject HPBarEnd { get; set; }
+    [field: SerializeField]
+    public GameObject HPBarMiddle { get; set; }
+    public HPBarHPDisplay[] HPBars { get; set; }
+
+    private Character ch;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ch = GameObject.Find("Character").GetComponent<Character>();
+        HP = ch.getHealthPoints;
+        currentHP = HP;
+        HPBars = new HPBarHPDisplay[HP];
+        for (int i = 0; i < HP; i++)
+        {
+            switch (i)
+            {
+                case 0:
+                    HPBars[i] = Instantiate(HPBarStart, transform, false).GetComponent<HPBarHPDisplay>();
+                    break;
+                case int n when (n == (HP - 1)):
+                    HPBars[i] = Instantiate(HPBarEnd, transform, false).GetComponent<HPBarHPDisplay>();
+                    break;
+                default:
+                    HPBars[i] = Instantiate(HPBarMiddle, transform, false).GetComponent<HPBarHPDisplay>();
+                    break;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentHP = ch.getHealthPoints;
+        for (int i = 0; i < HP; i++)
+        {
+            if (i < currentHP)
+            {
+                HPBars[i].hasHP = true;
+            }
+            else
+            {
+                HPBars[i].hasHP = false;
+            }
+        }
+
+    }
+}
